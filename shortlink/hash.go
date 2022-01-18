561006fe31345648a05998a8faff00d25d8b17fd
Jamie Flynn <gjf20@case.edu>
2022-01-18 12:27:27 -0700

    Hashes the url to get the short slug

--- shortlink/hash.go
+++ shortlink/hash.go
@@ -2,6 +2,7 @@ package shortlink
 
 import (
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 
 	"example.com/shortlink-kata/db"
@@ -12,17 +13,18 @@ const defaultHashLength = 7
 var isAlreadyCreated = db.Exists
 
 func generateHash(s string) (string, error) {
-	hash := sha1.Sum([]byte(s))
+	hash := sha1.Sum([]byte(s)) //outputs into hex format, but should be sufficient for our purposes
 
 	var potentialHash string
 	maxAdditionalChars := len(hash) - defaultHashLength
 	for i := 0; i <= maxAdditionalChars; i++ {
-		potentialHash = s[:defaultHashLength+i]
+		potentialHash = hex.EncodeToString(hash[:defaultHashLength+i])
+
 		if !isAlreadyCreated(potentialHash) {
 			break
 		}
 		if i == maxAdditionalChars {
-			return "", fmt.Errorf("Could not generate an unused hash for address %v", s)
+			return "", fmt.Errorf("could not generate an unused hash for address %v", s)
 		}
 	}
 
